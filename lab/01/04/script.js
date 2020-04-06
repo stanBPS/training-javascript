50b4c714912c96efb1cdddc0b539c19595527664
stanBPS <stanley.bruneau@univ-tlse3.fr>
2020-04-06 18:17:42 +0200

    externalisation js

---
+++ lab/01/04/script.js
@@ -0,0 +1,17 @@
+const name = "Bruneau Stanley";
+
+function displayBooleanTrue() {
+    console.log(true);
+}
+
+function displayName() {
+    console.log(name);
+}
+
+function displayNameUppercase() {
+    console.log(name.toUpperCase());
+}
+
+function displaySum() {
+    console.log(10+10.5);
+}
